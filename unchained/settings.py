ea6d3c6f7f58f8cb39d2810e6cddc77c773ef784
Aaron Hanson <spinnelein@gmail.com>
2022-07-07 20:31:28 -0700

    fix admin css

--- unchained/settings.py
+++ unchained/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'unchained/static')
 STATIC_URL = '/static/'
 LOGOUT_REDIRECT_URL = '/'
